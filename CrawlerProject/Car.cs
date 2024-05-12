01db01d045682ed5b6a75ee925911d6968230be7
DESKTOP-KRLCQFE\quoch <quochuy4307@gmail.com>
2024-05-12 18:01:08 +0700

    Add project files.

---
+++ CrawlerProject/Car.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CrawlerProject
+{
+    internal class Car
+    {
+        public string Model { get; set; }
+        public string Price { get; set; }
+        public string Currency { get; set; }
+        public string FuelType { get; set; }
+        public string NumberOfSeats { get; set; }
+        public string Origin { get; set; }
+        public string Designs { get; set; }
+        public string Features { get; set; }
+        public string Engine { get; set; }
+        public string Link { get; set; }
+        public string ImageUrl { get; set; }
+    }
+}
---
+++ CrawlerProject/Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HtmlAgilityPack;
+using System.Web;
+using System.Net;
+using System.IO;
+using OfficeOpenXml;
+
+namespace CrawlerProject
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            HtmlWeb htmlWeb = new HtmlWeb()
+            {
+                AutoDetectEncoding = true,
+                OverrideEncoding = Encoding.UTF8, //Hiển thị tiếng việt
+            };
+            var url = "https://www.toyota.com.vn/danh-sach-xe";
+            //HtmlDocument load trang web
+            HtmlDocument htmlDocument = htmlWeb.Load(url);
+            var cars = new List<Car>();
+            var divs = htmlDocument.DocumentNode.Descendants("div").Where(x => x.GetAttributeValue("Class", "").Equals("col-12 col-md-6 col-lg-4 product-show-item")).ToList();
+            //crawl dữ liệu
+            foreach ( var div in divs ) {
+                var car = new Car
+                {
+                    Model = div.Descendants("div").Where(x => x.GetAttributeValue("Class", "").Equals("product-item-content")).FirstOrDefault()
+                    .Descendants("h2").FirstOrDefault().InnerText,
+                    Price = div.Descendants("div").Where(x => x.GetAttributeValue("Class", "").Equals("product-item-content")).FirstOrDefault()
+                    .Descendants("span").Skip(1).FirstOrDefault().InnerText,
+                    Currency = div.Descendants("div").Where(x => x.GetAttributeValue("Class", "").Equals("product-item-content")).FirstOrDefault()
+                    .Descendants("span").Skip(2).FirstOrDefault().InnerText,
+                    Link = "https://www.toyota.com.vn" + div.Descendants("a").FirstOrDefault().ChildAttributes("href").FirstOrDefault().Value,
+                    ImageUrl = div.Descendants("img").FirstOrDefault().ChildAttributes("src").FirstOrDefault().Value,
+                    NumberOfSeats = div.Descendants("div").Where(x => x.GetAttributeValue("Class", "").Equals("content-product-description")).FirstOrDefault()
+                    .Descendants("p").FirstOrDefault().InnerText,
+                    Designs = div.Descendants("div").Where(x => x.GetAttributeValue("Class", "").Equals("content-product-description")).FirstOrDefault()
+                    .Descendants("p").Skip(1).FirstOrDefault().InnerText,
+                    FuelType = div.Descendants("div").Where(x => x.GetAttributeValue("Class", "").Equals("content-product-description")).FirstOrDefault()
+                    .Descendants("p").Skip(2).FirstOrDefault().InnerText,
+                    Origin = div.Descendants("div").Where(x => x.GetAttributeValue("Class", "").Equals("content-product-description")).FirstOrDefault()
+                    .Descendants("p").Skip(3).FirstOrDefault().InnerText,
+                    Features = div.Descendants("div").Where(x => x.GetAttributeValue("Class", "").Equals("content-product-description")).FirstOrDefault()
+                    .Descendants("p").Skip(4).FirstOrDefault().InnerText,
+                    Engine = div.Descendants("div").Where(x => x.GetAttributeValue("Class", "").Equals("content-product-description")).FirstOrDefault()
+                    .Descendants("p").Skip(5).FirstOrDefault().InnerText,
+                };
+                car.Features = WebUtility.HtmlDecode(car.Features);
+                car.Engine = WebUtility.HtmlDecode(car.Engine);
+                cars.Add(car);
+            }
+            //xuất ra excel
+            // If you are a commercial business and have
+            // purchased commercial licenses use the static property
+            // LicenseContext of the ExcelPackage class:
+            ExcelPackage.LicenseContext = LicenseContext.Commercial;
+
+            // If you use EPPlus in a noncommercial context
+            // according to the Polyform Noncommercial license:
+            ExcelPackage.LicenseContext = LicenseContext.NonCommercial;
+            var file = new FileInfo(@"D:\ExportExcelCrawlData\Sample.xlsx");
+            using(ExcelPackage excel = new ExcelPackage(file))
+            {
+                ExcelWorksheet excelWorksheet = excel.Workbook.Worksheets["sheet1"];
+                excelWorksheet.Cells.LoadFromCollection(cars, true);
+                FileInfo excelFile = new FileInfo(@"D:\ExportExcelCrawlData\Result.xlsx");
+                excel.SaveAs(excelFile);
+            }
+            Console.ReadLine();
+        }
+    }
+}
